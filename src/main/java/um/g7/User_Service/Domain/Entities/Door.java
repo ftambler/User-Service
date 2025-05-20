01f2e888b9da4768f0b51ee8d46ebea9069f5623
ftambler <federicotambler@gmail.com>
2025-05-20 18:04:30 -0300

    Unique constraint fix

--- src/main/java/um/g7/User_Service/Domain/Entities/Door.java
+++ src/main/java/um/g7/User_Service/Domain/Entities/Door.java
@@ -3,10 +3,10 @@ package um.g7.User_Service.Domain.Entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.checkerframework.common.aliasing.qual.Unique;
 
 import java.util.UUID;
 
@@ -14,12 +14,11 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = {"name"}) })
 public class Door {
 
     @Id
     private UUID id;
-    @Unique
     private String name;
     private int accessLevel;
 }

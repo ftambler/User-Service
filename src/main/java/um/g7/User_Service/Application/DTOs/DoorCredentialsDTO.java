d8a1ecd6e27b062628647d628b62b5b607a8cd25
ftambler <federicotambler@gmail.com>
2025-05-23 18:29:04 -0300

    fix door dto

--- src/main/java/um/g7/User_Service/Application/DTOs/DoorCredentialsDTO.java
+++ src/main/java/um/g7/User_Service/Application/DTOs/DoorCredentialsDTO.java
@@ -2,9 +2,11 @@ package um.g7.User_Service.Application.DTOs;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class DoorCredentialsDTO {
     private String doorName;
     private String passcode;

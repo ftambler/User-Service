9d0e8dca664d8d6e78560b7c877edf681bce5d49
spereira04 <santipereira1987@gmail.com>
2025-05-31 13:43:36 -0300

    delete user rfid & vector when removing user

--- src/main/java/um/g7/User_Service/Domain/Services/UserService.java
+++ src/main/java/um/g7/User_Service/Domain/Services/UserService.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Service;
 
 import um.g7.User_Service.Domain.Entities.UserEntity;
+import um.g7.User_Service.Domain.Entities.UserRFID;
+import um.g7.User_Service.Domain.Entities.UserVector;
 import um.g7.User_Service.Domain.Exceptions.UserNotFoundException;
+import um.g7.User_Service.Infrastructure.Repositories.UserRFIDRepository;
 import um.g7.User_Service.Infrastructure.Repositories.UserRepository;
+import um.g7.User_Service.Infrastructure.Repositories.UserVectorRepository;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -14,9 +18,13 @@ import java.util.UUID;
 public class UserService {
     
     private final UserRepository userRepository;
+    private final UserRFIDRepository userRFIDRepository;
+    private final UserVectorRepository userVectorRepository;
     
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserRFIDRepository userRFIDRepository, UserVectorRepository userVectorRepository) {
         this.userRepository = userRepository;
+        this.userRFIDRepository = userRFIDRepository;
+        this.userVectorRepository = userVectorRepository;
     }
 
     public void addUser(UserEntity user) {
@@ -28,6 +36,16 @@ public class UserService {
 
         if (optionalUser.isEmpty())
             throw new UserNotFoundException();
+
+        Optional<UserRFID> optionalUserRFID = userRFIDRepository.findById(userId);
+
+        if (optionalUserRFID.isPresent())
+            userRFIDRepository.deleteById(userId);
+
+        Optional<UserVector> optionalUserVector = userVectorRepository.findById(userId);
+
+        if (optionalUserVector.isPresent())
+            userVectorRepository.deleteById(userId);
 
         userRepository.deleteById(userId);
     }
